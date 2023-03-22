814ea62228e2a929498fbdc01c077892b57bda3f
unknown <kitharveycaubalejo@gmail.com>
2023-03-22 19:23:42 +0800

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,18 @@
 import Nav from "@/components/Nav";
 import "@/styles/style.scss";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<div className="wrapper">
-			<div className="container">
-				<Nav />
-				<Component {...pageProps} />
+		<>
+			<div className="wrapper">
+				<div className="container">
+					<Nav />
+					<Component {...pageProps} />
+				</div>
 			</div>
-		</div>
+			<Analytics />
+		</>
 	);
 }
