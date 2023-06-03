7ee158b4f183b5fd9e72b66507c1c05610610119
kitharvey <kitharveycaubalejo@gmail.com>
2023-06-03 23:46:07 +0800

    reduce grid, remove hook

--- components/Tiles.tsx
+++ components/Tiles.tsx
@@ -1,10 +1,31 @@
-import useTilesNumber from "@/hooks/useTilesNumber";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 interface TilesProps {}
 
+const getNumberOfTiles = () => {
+	const { clientWidth: width, clientHeight: height } = document.body;
+	const tileSize = 50;
+	const columns = Math.floor(width / tileSize);
+	const rows = Math.floor(height / tileSize);
+	return { columns, rows };
+};
+
 const Tiles: React.FC<TilesProps> = ({}) => {
-	const tiles = useTilesNumber();
+	const [tiles, setTiles] = useState<number>(0);
+
+	useEffect(() => {
+		function handleResize() {
+			const { columns, rows } = getNumberOfTiles();
+			document.documentElement.style.setProperty("--columns", columns + "");
+			document.documentElement.style.setProperty("--rows", rows + "");
+			setTiles(columns * rows);
+		}
+		handleResize();
+		window.addEventListener("resize", handleResize);
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
 
 	return (
 		<div className="tiles">
